2dd12071d5572eb5a2e8eab481264f6c6e4607c4
Christophe Bedard <bedard.christophe@gmail.com>
2020-08-29 13:18:46 -0400

    Add option for providing version checking command (#111)
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -9,6 +9,7 @@ beforeEach(() => {
   // otherwise an environment variable set in a test can creep into a later test
   process.env['INPUT_VERSION_REGEX'] = '^[0-9]+\\.[0-9]+\\.[0-9]+$';
   process.env['INPUT_VERSION_TAG_PREFIX'] = '';
+  process.env['INPUT_VERSION_ASSERTION_COMMAND'] = '';
   process.env['INPUT_ANNOTATED'] = 'false';
   process.env['INPUT_DRY_RUN'] = 'false';
   // Set token to simplify tests
@@ -150,6 +151,68 @@ describe('action', () => {
     // Outputs should be empty
     expect(stdout_write).toHaveBeenCalledWith(expect.stringMatching(/^.*name=tag::[\n]*$/));
     expect(stdout_write).toHaveBeenCalledWith(expect.stringMatching(/^.*name=commit::[\n]*$/));
+  });
+
+  it('works if the version assertion command works', async () => {
+    process.env['INPUT_VERSION_ASSERTION_COMMAND'] = 'true';
+
+    nock('https://api.github.com')
+      .get('/repos/theowner/therepo/git/commits/0123456789abcdef')
+      .reply(200, {
+        message: '3.4.3'
+      });
+    nock('https://api.github.com')
+      .post('/repos/theowner/therepo/git/refs', {ref: 'refs/tags/3.4.3', sha: '0123456789abcdef'})
+      .reply(201, {});
+
+    const stdout_write = jest.spyOn(process.stdout, 'write');
+
+    await run();
+
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringContaining('name=tag::3.4.3'));
+    expect(stdout_write).toHaveBeenCalledWith(
+      expect.stringContaining('name=commit::0123456789abcdef')
+    );
+  });
+
+  it('works if the version assertion command works, replacing $version with the version', async () => {
+    process.env['INPUT_VERSION_ASSERTION_COMMAND'] = '[ "$version" == "3.4.4" ]';
+
+    nock('https://api.github.com')
+      .get('/repos/theowner/therepo/git/commits/0123456789abcdef')
+      .reply(200, {
+        message: '3.4.4'
+      });
+    nock('https://api.github.com')
+      .post('/repos/theowner/therepo/git/refs', {ref: 'refs/tags/3.4.4', sha: '0123456789abcdef'})
+      .reply(201, {});
+
+    const stdout_write = jest.spyOn(process.stdout, 'write');
+
+    await run();
+
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringContaining('name=tag::3.4.4'));
+    expect(stdout_write).toHaveBeenCalledWith(
+      expect.stringContaining('name=commit::0123456789abcdef')
+    );
+  });
+
+  it('fails if the version assertion command fails', async () => {
+    process.env['INPUT_VERSION_ASSERTION_COMMAND'] = 'false';
+
+    nock('https://api.github.com')
+      .get('/repos/theowner/therepo/git/commits/0123456789abcdef')
+      .reply(200, {
+        message: '3.4.5'
+      });
+
+    const stdout_write = jest.spyOn(process.stdout, 'write');
+
+    await run();
+
+    expect(stdout_write).toHaveBeenCalledWith(
+      expect.stringContaining('::error::Version assertion failed')
+    );
   });
 
   it('creates an annotated tag if the option is enabled', async () => {
