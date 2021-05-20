6fa062c19e0ddb6f98f3a06448e9b5cccfa72016
Christophe Bedard <bedard.christophe@gmail.com>
2021-05-20 16:30:57 -0400

    Add test cases checking entire commit message for version match
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -12,6 +12,7 @@ beforeEach(() => {
   process.env['INPUT_VERSION_REGEX'] = '^[0-9]+\\.[0-9]+\\.[0-9]+$';
   process.env['INPUT_VERSION_TAG_PREFIX'] = '';
   process.env['INPUT_VERSION_ASSERTION_COMMAND'] = '';
+  process.env['INPUT_CHECK_ENTIRE_COMMIT_MESSAGE'] = 'false';
   process.env['INPUT_ANNOTATED'] = 'false';
   process.env['INPUT_DRY_RUN'] = 'false';
   // Set token to simplify tests
@@ -290,6 +291,69 @@ describe('action', () => {
     expect(stdout_write).toHaveBeenCalledWith(
       expect.stringContaining('::error::Version assertion failed')
     );
+  });
+
+  it('checks the entire commit message for a matching version if the option is enabled', async () => {
+    process.env['INPUT_VERSION_REGEX'] = String.raw`Version: ([0-9]+\.[0-9]+\.[0-9]+)`;
+    process.env['INPUT_CHECK_ENTIRE_COMMIT_MESSAGE'] = 'true';
+
+    nock('https://api.github.com')
+      .get('/repos/theowner/therepo/git/commits/0123456789abcdef')
+      .reply(200, {
+        message: 'Tag new version\n\nVersion: 1.5.2\nsome more commit body text'
+      });
+    nock('https://api.github.com')
+      .post('/repos/theowner/therepo/git/refs', {ref: 'refs/tags/1.5.2', sha: '0123456789abcdef'})
+      .reply(201, {});
+
+    const stdout_write = jest.spyOn(process.stdout, 'write');
+
+    await run();
+
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringContaining('name=tag::1.5.2'));
+    expect(stdout_write).toHaveBeenCalledWith(
+      expect.stringContaining('name=commit::0123456789abcdef')
+    );
+  });
+
+  it('does not check the entire commit message for a matching version if the option is disabled', async () => {
+    process.env['INPUT_VERSION_REGEX'] = String.raw`Version: ([0-9]+\.[0-9]+\.[0-9]+)`;
+    process.env['INPUT_CHECK_ENTIRE_COMMIT_MESSAGE'] = 'false';
+
+    nock('https://api.github.com')
+      .get('/repos/theowner/therepo/git/commits/0123456789abcdef')
+      .reply(200, {
+        message: 'Tag new version\n\nVersion: 1.5.3\nsome more commit body text'
+      });
+
+    const stdout_write = jest.spyOn(process.stdout, 'write');
+
+    await run();
+
+    // Outputs should be empty
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringMatching(/^.*name=tag::[\n]*$/));
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringMatching(/^.*name=message::[\n]*$/));
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringMatching(/^.*name=commit::[\n]*$/));
+  });
+
+  it('does not do anything if there is no match when the option for checking entire commit message is enabled', async () => {
+    process.env['INPUT_VERSION_REGEX'] = String.raw`Version: ([0-9]+\.[0-9]+\.[0-9]+)`;
+    process.env['INPUT_CHECK_ENTIRE_COMMIT_MESSAGE'] = 'true';
+
+    nock('https://api.github.com')
+      .get('/repos/theowner/therepo/git/commits/0123456789abcdef')
+      .reply(200, {
+        message: 'Some commit title\n\nBlah blah\nsome more commit body text'
+      });
+
+    const stdout_write = jest.spyOn(process.stdout, 'write');
+
+    await run();
+
+    // Outputs should be empty
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringMatching(/^.*name=tag::[\n]*$/));
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringMatching(/^.*name=message::[\n]*$/));
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringMatching(/^.*name=commit::[\n]*$/));
   });
 
   it('creates an annotated tag if the option is enabled', async () => {
